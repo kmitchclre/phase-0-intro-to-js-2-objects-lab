32dda3f8a5780775f5a2d245926cdc540b781a5d
Kenny Clare <kmitchellclare@gmail.com>
2022-04-27 09:45:27 -0700

    commit tree b9265e891dade4919d3c37f98a1dbd29f44477c3

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Kenny",
+    streetAddress: "501 West 11th St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Alternate using ES6 Spread operators:
+
+    return { ...employee, [key]: value };
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = Object.assign({}, employee);
+    delete newObj[key];
+    return newObj;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+
+    return employee;
+}
